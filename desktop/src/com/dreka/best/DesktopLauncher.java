c374915714658c7142577e7d5600ed198af866ee
Glega <phptoster@yandex.ru>
2023-03-31 22:06:29 +0300

    Initial commit

---
+++ desktop/src/com/dreka/best/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.dreka.best;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.dreka.best.MainScreen;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("AnimationTest");
+		new Lwjgl3Application(new MainScreen(), config);
+	}
+}
